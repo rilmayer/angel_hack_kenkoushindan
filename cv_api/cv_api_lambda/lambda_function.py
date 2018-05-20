24e2dd89e51b3c8375f928da7c41158b2d849a27
Kosuke Kitahara <k_kitahara@kk.local>
2018-05-20 11:25:52 +0900

    update 0520 11:25

--- cv_api/cv_api_lambda/lambda_function.py
+++ cv_api/cv_api_lambda/lambda_function.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import os
+import re
 
 API_KEY = os.getenv("DR_MEDICAL_CHECKUP_API_KEY", "api key not exist")
 
@@ -19,7 +20,14 @@ DETECTION_TYPES = [
 def lambda_handler(event, context):
     data = generate_json_from_base64_image(event['image'])
     response = get_response_from_cv_api(data)
-    return response.text
+    text_result = json.loads(response.text)
+    description = text_result['responses'][0]['textAnnotations'][0]['description']
+    parsed = parse_description(description)
+    advice = generate_advice(parsed)
+    return advice
+
+    # res_sapmple = 'コレステロールが高そうです'
+    # return res_sapmple
 
 
 def generate_json_from_base64_image(base64_image):
@@ -33,12 +41,13 @@ def generate_json_from_base64_image(base64_image):
     content_json_obj = {'content': base64_image}
 
     # Detection type.
-    detection_type = 5
-    max_results = 30
-    feature_json_obj = [{
-        'type': get_detection_type(detection_type),
-        'maxResults': max_results,
-    }]
+    # detection_type = 5
+    # max_results = 30
+    feature_json_obj = [{'type': get_detection_type(5),
+                         'maxResults': 50,
+                        }
+                       ]
+
 
     # Now add it to the request
     request_list = []
@@ -74,3 +83,68 @@ def get_response_from_cv_api(data):
     response = requests.post(url=url, data=data, headers={'Content-Type': 'application/json'})
 
     return response
+
+
+def parse_description(description):
+    """ Parse description.
+    Args:
+        description: str.
+    """
+    parsed = description.split('\n')
+    return parsed
+
+
+def parse_description(description):
+    """ Parse description.
+    Args:
+        description: str.
+    """
+    parsed = description.split('\n')
+    return parsed
+
+
+JARGON_MAP = {'hdlc': 'HDLコレステロール (善玉コレステロール): 血管内のコレステロールを回収する役割があります'}
+ADVICE_MAP = {'hdlc_low': '善玉コレステロール＝血管内のコレステロールを回収する役割があります'}
+
+
+def generate_advice(parsed):
+    """ Generate advice.
+    Args:
+        parsed: list. parsed ocr text.
+    Returns:
+        advice: string. advice for health.
+    """
+    # Get item, standard and actual val.
+    ind = 32
+    row = parsed[ind]
+    item, l_bound, h_bound = row.split('-')
+    l_bound = int(l_bound)
+    h_bound = int(h_bound)
+    item = item[2:]
+    actual = parsed[ind + 2]
+    act, _ = actual.split(' ')
+    act = int(act)
+
+    # CREATE MAP LATER
+    reformed_item = ''
+    if item == 'H D L . C':
+        reformed_item = 'HDL.C'
+
+    advice = ''
+    if reformed_item == 'HDL.C' and l_bound <= act and act <= h_bound:
+        advice = '問題ありません'
+    elif reformed_item == 'HDL.C' and l_bound >= act:
+        advice = """
+        HDL.Cが基準値を下回っています（基準値: {} - {}, あなたの値: {}）。
+        【結果の解釈】
+        HDLコレステロールは善玉コレステロールと呼ばれ、血管内の脂質を回収する役割があります。HDLコレステロールが低いと、血管内に脂肪が増え、動脈硬化などを引き起こします。
+        【改善策】
+        薬ではなく、まずは運動と食事を改善します。運動とは、例えば週に2,3日に30分運動する時間を設けることが推奨されます。食事は油物を避けることが勧められます。
+        【今後の方針】
+        運動・食事習慣を改善して半年〜1年後に再度検査を行い、HDLコレステロールが改善していないようであれば医療機関での詳しい検査を勧めます。
+        【先生から一言】
+        そこまで心配する必要はないので安心してください。
+        """.format(l_bound, h_bound, act)
+    print(advice)
+    return advice
+
